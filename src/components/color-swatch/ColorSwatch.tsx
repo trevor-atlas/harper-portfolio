54aa7e86b31af5fb8fd504fa2ba09816ca984fb9
Trevor Atlas <me@trevoratlas.com>
2022-04-27 23:11:47 -0400

    add a thing

--- src/components/color-swatch/ColorSwatch.tsx
+++ src/components/color-swatch/ColorSwatch.tsx
@@ -1,9 +1,12 @@
+import { HoverTilt } from "../hover-tilt/HoverTilt";
 
 export const ColorSwatch = ({ color, colorLabel, label }) => (
-	<div className="colorswatch" style={{backgroundColor: color }}>
-		<div className="content">
-			<div className="label">{ label }</div>
-			<div className="color">{colorLabel}</div>
+	<HoverTilt>
+		<div className="colorswatch" style={{ backgroundColor: color }}>
+			<div className="content">
+				<div className="label">{label}</div>
+				<div className="color">{colorLabel}</div>
+			</div>
 		</div>
-	</div>
+	</HoverTilt>
 );
